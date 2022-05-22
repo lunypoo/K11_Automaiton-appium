1ef0ec4aa421ed91e0a7a0c50a3b498e5942bded
KhanhLinhDao0102 <63227020+lunypoo@users.noreply.github.com>
2022-05-23 01:19:32 +0700

    Lab2_1

--- src/Lab_02/Lab2_1.java
+++ src/Lab_02/Lab2_1.java
@@ -2,34 +2,35 @@ package Lab_02;
 
 import java.util.Scanner;
 
-public class lab2_1 {
+public class Lab2_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please input height: ");
+        System.out.println("height: ");
         float height = scanner.nextFloat();
-        System.out.println("Please input weight: ");
+        System.out.println("weight: ");
         float weight = scanner.nextFloat();
 
-        float BMI = weight / (height * 2);
-        System.out.printf("Your BMI is: %f\n ", BMI);
+        float BMI;
+        BMI = weight / (height * 2);
+        System.out.println("Your BMI is: "+BMI+"");
 
-        if (BMI <= 18.5){
+        if (BMI < 18.5){
             System.out.println("Underweight");
             float a;
             a = (float) (18.5 - BMI);
-            System.out.println("Ban can tang them: "+a+"kg");
+            System.out.println("Ban can tang:"+a+"kg");
         } else if (BMI <= 24.9) {
             System.out.println("Normal weight");
-        } else if (BMI < 29.9) {
+        } else if (BMI <= 29.9) {
             System.out.println("Overweight");
             float b;
             b = (float) (BMI - 24.9);
-            System.out.println("Ban can giam: "+b+"kg");
-        } else {
+            System.out.println("Ban can phai giam:"+b+"kg");
+        } else{
             System.out.println("Obesity");
             float c;
-            c =  (float) (BMI - 24.9);
-            System.out.println("Ban can phai giam: "+c+"kg");
+            c = (float) (BMI - 24.9);
+            System.out.println("Ban can phai giam :"+c+"kg");
         }
     }
 }
