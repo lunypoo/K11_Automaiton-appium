dc95375bc66d1147c98871abada5c7e685cd34b0
KhanhLinhDao0102 <63227020+lunypoo@users.noreply.github.com>
2022-05-26 18:40:20 +0700

    Lab3_4

--- src/Lab_03/Lab3_4.java
+++ src/Lab_03/Lab3_4.java
@@ -1,12 +1,33 @@
 package Lab_03;
 
+import java.util.Arrays;
+
 public class Lab3_4 {
-//merge 2 sorted into 1 sorted
+
     public static void main(String[] args) {
-        int[] arr1 = new int[] {1,12,16,28,34};
-        int[] arr2 = new int[] {1,13,16,27,99};
-        int i = 0;
+        int[] arr1 = {1, 12, 16, 28, 34};
+        int[] arr2 = {1, 13, 16, 27, 99};
+        int length = arr1.length + arr2.length;
+        int[] result = new int[length];
+        int pos = 0;
+        for (int element : arr1) {
+            result[pos] = element;
+            pos++;
+        }
+        for (int element : arr2) {
+            result[pos] = element;
+            pos++;
+        }
+        for (int i = 0; i < result.length; i++) {
+            for (int j = i + 1; j < result.length; j++) {
+                if (result[i] > result[j]) {
+                    int temp = result[j];
+                    result[j] = result[i];
+                    result[i] = temp;
+                }
 
-
+            }
+        }
+        System.out.println(Arrays.toString(result));
     }
 }
